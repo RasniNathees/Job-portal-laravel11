16e29aff9e067b2e618152fc8946b2c40503baa3
Rasni Nathees <rasninathees.se@gmail.com>
2024-07-31 16:02:55 +0530

    tests

--- tests/Unit/Models/JobTest.php
+++ tests/Unit/Models/JobTest.php
@@ -3,7 +3,14 @@
 use App\Models\Employer;
 use App\Models\Job;
 use App\Models\Tag;
+use Illuminate\Database\QueryException;
 
+
+ 
+it('Can handle jobs with no employer', function () {
+   Job::factory()->create(['employer_id' => null]);
+ })->throws(QueryException::class);
+ 
 it('Is Job has employer', function () {
    $employer = Employer::factory()->create();
    $job = Job::factory()->create(['employer_id'=>$employer->id]);
@@ -12,7 +19,7 @@ it('Is Job has employer', function () {
 });
 
 it("Is Job has a specific tag",function(){
-   $job = Job::factory()->create();
+   $job = Job::factory()->create(['salary'=>"45568"]);
    $tag = Tag::factory()->create();
    $job->tags()->attach($tag);
    expect($job->tags->contains($tag->id))->toBeTrue();
@@ -23,4 +30,21 @@ it("Is Job has a correct tag count", function(){
    $tag = Tag::factory()->create();
    $job->tags()->attach($tag);
    expect($job->tags)->toHaveCount(1);
+});
+
+it('Can Job have multiple tags', function () {
+   $job = Job::factory()->create();
+   $tags = Tag::factory()->count(3)->create();
+   $job->tags()->attach($tags);
+   
+   expect($job->tags->count())->toBe(3);
+});
+
+
+it('can detach a tag from a job', function () {
+   $job = Job::factory()->create();
+   $tag = Tag::factory()->create();
+   $job->tags()->attach($tag);
+   $job->tags()->detach($tag);
+   expect($job->tags->contains($tag->id))->toBeFalse();
 });
