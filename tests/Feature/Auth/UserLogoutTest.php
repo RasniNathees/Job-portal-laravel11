98bc2e95f75fa115644f57dea83b29bb44f84d13
Rasni Nathees <rasninathees.se@gmail.com>
2024-08-01 17:58:27 +0530

    logout test

--- tests/Feature/Auth/UserLogoutTest.php
+++ tests/Feature/Auth/UserLogoutTest.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 it('User can logout',function(){
     $user =User::factory()->create();
     expect($user)->toBeRedirectFor('/logout','post');
-    
+    expect(Auth::check())->toBeFalse();
 });
